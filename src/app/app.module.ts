b1855ca34ec80f81eb3526ee6ed3dfbdf1d220e9
Marcin <marcin15g@gmail.com>
2020-11-13 20:35:15 +0100

    started working on test creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 //Angular Material 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatRippleModule} from '@angular/material/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -30,10 +36,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatRippleModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
     
   ],
   providers: [],
--- src/app/create-test/create-test.component.ts
+++ src/app/create-test/create-test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-test',
@@ -7,9 +8,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTestComponent implements OnInit {
 
-  constructor() { }
+  dynamicForm: FormGroup;
+  submitted = false;
+  visibleQuestions = 1;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  get f() { return this.dynamicForm.controls; }
+  get t() { return this.f.questions as FormArray; }
 
   ngOnInit(): void {
+    this.dynamicForm = this.formBuilder.group({
+      numberOfQuestions: [''],
+      numberOfTestQuestions: [''],
+      questions: new FormArray([])
+    })
+
+    const test = this.t;
+    console.log(test);
+  }
+
+  addQuestion() {
+    this.t.push(this.formBuilder.group({
+      question: ['', Validators.required],
+      answer1: [''],
+      answer2: [''],
+      answer3: [''],
+      answer4: ['']
+    }))
+    this.f.numberOfQuestions.setValue(this.t.length);
+  }
+
+  onChangeQuestions(e) {
+    const numberOfQuestions = e.target.value || 0;
+    if(this.t.length < numberOfQuestions) {
+      for(let i = this.t.length; i < numberOfQuestions; i++) {
+        this.t.push(this.formBuilder.group({
+          question: ['', Validators.required],
+          answer1: [''],
+          answer2: [''],
+          answer3: [''],
+          answer4: ['']
+        }))
+      }
+    } else {
+      for (let i = this.t.length; i >= numberOfQuestions; i--) {
+        this.t.removeAt(i);
+      }      
+    }
+    console.log(this.t);
+    console.log(this.f);
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if(this.dynamicForm.invalid) return;
+    console.log('Submitted!', this.dynamicForm.value);
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.dynamicForm.reset();
+    this.t.clear();
+  }
+
+  onClear() {
+    this.submitted = false;
+    this.t.reset();
   }
 
 }
