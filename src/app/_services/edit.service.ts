18bcfb0feddf41be97d135f187d0e8f1d1d9d345
Marcin <marcin15g@gmail.com>
2020-11-22 00:39:46 +0100

    Server url added

--- src/app/_services/edit.service.ts
+++ src/app/_services/edit.service.ts
@@ -8,10 +8,16 @@ export class EditService {
 
   constructor(private http: HttpClient) { }
 
-  serverUrl: string = 'TODOTODO';
+  serverUrl: string = 'https://online-tests2137.herokuapp.com';
 
   getTest(testID: string) {
     //TODOTODO
-    return '{"title":"ASDASD","user_id":123,"num_of_questions":"1","questions":[{"question":"Questionssss","required":true,"answers":[{"answer":"ASD","correct":true},{"answer":"DSA","correct":true},{"answer":"SAD","correct":false},{"answer":"DAS","correct":false}]},{"question":"SECONDDD","required":false,"answers":[{"answer":"DASD","correct":false},{"answer":"ASDASD","correct":false},{"answer":"DASDASD","correct":true},{"answer":"ASDASD","correct":true}]}]}'
+    //TEST RESPONSE
+    //return '{"title":"ASDASD","user_id":123,"num_of_questions":"1","questions":[{"question":"Questionssss","required":true,"answers":[{"answer":"ASD","correct":true},{"answer":"DSA","correct":true},{"answer":"SAD","correct":false},{"answer":"DAS","correct":false}]},{"question":"SECONDDD","required":false,"answers":[{"answer":"DASD","correct":false},{"answer":"ASDASD","correct":false},{"answer":"DASDASD","correct":true},{"answer":"ASDASD","correct":true}]}]}'
+    return this.http.get<{test: object}>(this.serverUrl + '/test/' + testID);
+  }
+
+  editTest(testID: string, test: object) {
+    return true;
   }
 }
--- src/app/_services/upload.service.ts
+++ src/app/_services/upload.service.ts
@@ -8,9 +8,9 @@ export class UploadService {
 
   constructor(private http: HttpClient) { }
 
-  serverUrl: string = 'TODOTODO';
+  serverUrl: string = 'https://online-tests2137.herokuapp.com';
 
   uploadTest(test) {
-    return this.http.post<{res: string}>(this.serverUrl + '/upload', {test: test});
+    return this.http.post<any>(this.serverUrl + '/test', {test: test});
   }
 }
