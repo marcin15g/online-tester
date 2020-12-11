377a9c29ce2a9d87439bac496262841bc28cdcf8
Marcin Grzebyk <marcin.grzebyk@justpremium.com>
2020-12-11 15:18:02 +0100

    Error when too many test questions has been set

--- src/app/_components/create-test/create-test.component.ts
+++ src/app/_components/create-test/create-test.component.ts
@@ -1,24 +1,12 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import { FormBuilder, FormGroup, FormArray, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router'
 import { EditService } from '../../_services/edit.service';
 import { UploadService } from '../../_services/upload.service';
 import { Popup } from './popup/popup';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
-
-export function validateNumOfQuestions(
-  control: AbstractControl
-): ValidationErrors | null {
-  if(control && control.get("numOfQuestions") && control.get("numOfTestQuestions")) {
-    const num = control.get("numOfQuestions").value;
-    const testNum = control.get("numOfTestQuestions").value;
-    
-    return (testNum > num) ? {numOfQuestionsError: true} : null
-  }
-  return null;
-}
+import { validateNumOfQuestions } from '../../_helpers/validator';
 
 @Component({
   selector: 'app-create-test',
---
+++ src/app/_helpers/validator.ts
@@ -0,0 +1,19 @@
+import { AbstractControl, ValidationErrors } from "@angular/forms";
+
+export function validateNumOfQuestions(
+    control: AbstractControl
+  ): ValidationErrors | null {
+    if(control && control.get("numOfQuestions") && control.get("numOfTestQuestions")) {
+      const num = control.get("numOfQuestions").value;
+      const testNum = control.get("numOfTestQuestions").value;
+
+      if(testNum > num) {
+        const testControl = control.get('numOfTestQuestions');
+        if(testControl) testControl.setErrors({'moreThanNum': true});
+        return {numOfQuestionsError: true};
+      } else {
+        return null;
+      }
+    }
+    return null;
+  }
